cd0c62ed5284dd1068a7f85258f421edb86b9ab1
YasukoPooh <87926800+YasukoPooh@users.noreply.github.com>
2025-05-09 16:21:18 +0900

    Update script-preload-audio.js

--- script-preload-audio.js
+++ script-preload-audio.js
@@ -78,7 +78,7 @@ document.getElementById('startButton').addEventListener('click', () => {
 
   setTimeout(() => {
     startTimer();
-  }, 2000);
+  }, 4000);
 });
 
 document.getElementById('skipButton').addEventListener('click', () => {
